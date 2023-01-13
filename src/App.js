ba8387be3de0e43fdf8717b92fa4e7e801cc08b4
DUSTINRYOO <hyeony77@gmail.com>
2023-01-12 23:23:59 -0500

    Basics for React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (evemt) => setKeyword(evemt.target.value);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log({ counter });
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>Let's get it</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search"
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
